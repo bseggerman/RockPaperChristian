5b65e99aa14863888ffb55811b00930add7c80d7
bseggerman <bjseggerman@yahoo.com>
2024-08-28 15:50:35 -0500

    get AI and player choice work

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 /*
 
-playing in the console so no need for html or css
-
 prompt input from player
 
 compare input to random input 
@@ -25,43 +23,21 @@ add score to winner of round
 games stops after player or random gets to 3
 
 */
-
 let aiChoice;
-let rock = 1;
-let christian = 2;
-let paper = 3;
+let playerChoice;
+let options = ["Neato", "Rock", "Christian", "Paper"];
 
 function getAiChoice() {
-       
-    
-    let aiChoice = Math.floor(Math.random() * 3) + 1; 
-    
-    if (aiChoice === 1) {
-        return "AI chooses Rock";
-    } else if (aiChoice === 2) {
-        return "AI chooses Christian";
-    } else {
-        return "AI chooses Paper";
-    }
+    let aiChoice = Math.floor(Math.random() * 3) + 1;
+    return options[(aiChoice)];
 }
 console.log(getAiChoice());
-
-let playerChoice;
-
 function getPlayerChoice() {
     let playerChoice = prompt("Make your choice.")
-    return "You chose " + playerChoice + ".";
+    return playerChoice;
 }
-console.log(getPlayerChoice());
 
-let playerScore = 0;
 let aiScore = 0;
+let playerScore = 0;
 
-function playRound(playerChoice, aiChoice) {
-        if (playerChoice === aiChoice) {
-            return "Both players choose Rock. Choose again.";
-        } else {
-            return "Test";
-        }
-}
-console.log(playRound());
+console.log(getPlayerChoice());
